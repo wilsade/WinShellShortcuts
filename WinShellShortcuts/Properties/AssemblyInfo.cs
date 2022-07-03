8d555b5b9c46ebcbd02036e85369625848517b8c
wilsade <wilsade@msn.com>
2022-07-03 13:36:02 -0300

    Update AssemblyInfo.cs

--- WinShellShortcuts/Properties/AssemblyInfo.cs
+++ WinShellShortcuts/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Teste 01
+// Teste 012
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
